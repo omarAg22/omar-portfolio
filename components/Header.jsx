01cd9205f54d24267e6aa5cc5da2f81e15cc3dc3
Omar AGTIB <22400016@moss.center>
2024-12-19 16:42:57 +0100

    header desgin

---
+++ components/Header.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+//components
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white ">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Agtib<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/* desktop navBar && hire me button */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+        {/* mobile navBar*/}
+
+        <div className="xl:hidden">mobile nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
